1abdb91e68caaf38bb8d0e4b18f3b01fa3e81433
Luca Staudt <staudt-business@web.de>
2022-08-11 10:18:23 +0200

    Design angepasst

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons";
-import { toast } from "react-toastify";
+import { UilSearch } from "@iconscout/react-unicons";
 
 function Inputs({ setQuery, units, setUnits }) {
   const [city, setCity] = useState("");
@@ -14,25 +13,9 @@ function Inputs({ setQuery, units, setUnits }) {
     if (city !== "") setQuery({ q: city });
   };
 
-  const handleLocationClick = () => {
-    if (navigator.geolocation) {
-      toast.info("Fetching users location.");
-      navigator.geolocation.getCurrentPosition((position) => {
-        toast.success("Location fetched!");
-        let lat = position.coords.latitude;
-        let lon = position.coords.longitude;
-
-        setQuery({
-          lat,
-          lon,
-        });
-      });
-    }
-  };
-
   return (
     <div className="flex flex-row justify-center my-6">
-      <div className="flex flex-row max-w-screen-lg items-center justify-center space-x-4">
+      <div className="flex flex-row max-w-full items-center justify-center space-x-4">
         <input
           value={city}
           onChange={(e) => setCity(e.currentTarget.value)}
@@ -42,17 +25,12 @@ function Inputs({ setQuery, units, setUnits }) {
         />
         <UilSearch
           size={25}
-          className="text-white cursor-pointer transition ease-out hover:scale-125"
+          className="text-white cursor-pointer transition ease-out hover:scale-125 "
           onClick={handleSearchClick}
-        />
-        <UilLocationPoint
-          size={25}
-          className="text-white cursor-pointer transition ease-out hover:scale-125"
-          onClick={handleLocationClick}
         />
       </div>
 
-      <div className="flex flex-row w-1/4 items-center justify-center">
+      <div className="flex flex-row w-2/3 items-center space-x-1.5  justify-center">
         <button
           name="metric"
           className="text-xl text-white font-light transition ease-out hover:scale-125"
@@ -60,7 +38,7 @@ function Inputs({ setQuery, units, setUnits }) {
         >
           °C
         </button>
-        <p className="text-xl text-white mx-1">|</p>
+        <p className="text-xl text-white mx-2">|</p>
         <button
           name="imperial"
           className="text-xl text-white font-light transition ease-out hover:scale-125"
